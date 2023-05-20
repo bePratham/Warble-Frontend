810196ac84e1e02dd67cbcf04f36728ca0b86e8a
varunbagga19 <varunbagga19@gmail.com>
2023-05-21 00:34:10 +0530

    Created Icon Button Component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,18 +1,8 @@
 import { View,Text,Image,StyleSheet} from "react-native";
 import {TweetType} from "../types";
 import { Entypo,EvilIcons } from '@expo/vector-icons';
-type IconButtonProps = {
-  icon: React.ComponentProps<typeof EvilIcons>['name'];
-  text ?: string | number,
-}
-const IconButton =({icon ,text}:IconButtonProps)=>{
-    return(
-      <View  style={{flexDirection:'row'}}>
-      <EvilIcons  name={icon}size={22} color="gray" />
-      <Text style={{fontSize:12 ,color:'gray'}}>{text } </Text>
-  </View>
-    )
-}
+import IconButton from "./IconButton";
+
 
 type TweetProps = {
     tweet: TweetType;
