619c286f75e12de77a5c87a8663da709a144082f
bepratham <prathamsinghkathayat@email.com>
2023-05-20 23:58:23 +0530

    j

---
+++ components/IconButon.tsx
@@ -0,0 +1,16 @@
+import {View,Text} from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
+
+type IconButtonProps = {
+    icon: React.ComponentProps<typeof EvilIcons>['name'];
+    text ?: string | number,
+  }
+  const IconButton =({icon ,text}:IconButtonProps)=>{
+      return(
+        <View  style={{flexDirection:'row'}}>
+        <EvilIcons  name={icon}size={22} color="gray" />
+        <Text style={{fontSize:12 ,color:'gray'}}>{text } </Text>
+    </View>
+      )
+  }
+  export default IconButton;
