e02d7e617c6239d8515b77c3837d0cf6c7e98e94
bepratham <prathamsinghkathayat@email.com>
2023-05-22 04:41:56 +0530

    First four hours of work

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
-
+import { withLayoutContext} from "expo-router";
+import { DrawerContentScrollView, DrawerItemList, createDrawerNavigator } from "@react-navigation/drawer";
+const DrawerNavigator= createDrawerNavigator().Navigator;
+const Drawer=withLayoutContext(DrawerNavigator); 
+import { Text } from "react-native";
+export const unstable_settings = {
+    initialRouteName: '(tabs)',
+  };
+  function CustomDrawerContent(props){
+    return(
+      <DrawerContentScrollView{...props}>
+      <Text style={{alignSelf:'center',fontSize:20}}>Pratham</Text>
+      <DrawerItemList{...props}/>
+      </DrawerContentScrollView>
+    )
+  }
 export default function DrawerLayout(){
-    return <Stack />;
+    return <Drawer drawerContent={(props)=><CustomDrawerContent{...props}/>}>
+        <Drawer.Screen name='(tabs)' options={{ headerShown:false ,title :'Home'}} />
+        <Drawer.Screen name='bookmarks' options={{title :'Bookmarks'}} />
+        <Drawer.Screen name='twitter-blue' options={{title :'Twitter Blue'}} />
+        </Drawer>;
 }
