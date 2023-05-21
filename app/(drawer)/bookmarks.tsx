1f989f815dc3452e798a568209559bcb6c7319a4
bepratham <prathamsinghkathayat@email.com>
2023-05-22 01:36:45 +0530

    drawer comingup

---
+++ app/(drawer)/bookmarks.tsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+
+const Bookmarks=()=>{
+    return (
+        <View>
+            <Text>bookmarks</Text>
+        </View>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,7 +12,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: '(drawer)',
 };
 
 export default function RootLayout() {
@@ -42,6 +42,7 @@ function RootLayoutNav() {
     <>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
+          <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
           <Stack.Screen name="tweet/[id]" options={{title:'Tweet'}}/>
