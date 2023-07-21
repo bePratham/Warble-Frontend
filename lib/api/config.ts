f368c20567c040f16c82912c1fccbfacb5944823
bepratham <prathamsinghkathayat@email.com>
2023-07-22 02:55:02 +0530

    merge into single git

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,2 +1,3 @@
-export const API_URL= "http://localhost:3000";
+// export const API_URL= "http://localhost:3000";
 // export const API_URL= "http://192.168.1.5:3000";
+export const API_URL= "http://192.168.1.4:19000";
